8558e520c77b5762ae5a367ba4677b058d921b17
Elzoka <mahmoud_elzoka@yahoo.com>
2018-12-04 23:07:49 +0200

    Adding Configuration

---
+++ RESTfulAPI/config.js
@@ -0,0 +1,31 @@
+/*
+*
+*
+* Create and export configuration variables
+*
+*/
+
+// Container for all the environments
+
+let environments = {};
+
+// Staging (defaut) environment
+environments.staging = {
+    port: 3000,
+    envName: 'staging'
+};
+
+// Production environment
+environments.production = {
+    port: 5000,
+    envName: 'production'
+}
+
+// Determine which environment was passed as a command-line argument
+const currentEnvironment = typeof(process.env.NODE_ENV) == 'string' ? process.env.NODE_ENV.toLowerCase() : '';
+
+// Check that the current environment is one of the environments above, if not, default to staging
+const environmentToExport = typeof(environments[currentEnvironment]) == 'object' ? environments[currentEnvironment] : environments.staging;
+
+// Export the module
+module.exports = environmentToExport;
--- RESTfulAPI/index.js
+++ RESTfulAPI/index.js
@@ -8,7 +8,8 @@
 // Dependencies
 const http = require('http');
 const url = require('url');
-const {StringDecoder} = require('string_decoder')
+const {StringDecoder} = require('string_decoder');
+const config = require('./config');
 
 
 // The server should respond to all requests with a string
@@ -74,8 +75,8 @@ const server = http.createServer((req, res) => {
 
 
 // Start the server, and have it listen on port 3000
-server.listen(3000, () => {
-    console.log("The Server is listening on port 3000");
+server.listen(config.port, () => {
+    console.log(`The Server is listening on port ${config.port} in ${config.envName} mode`);
 });
 
 const handlers = {};
