b8f4187b363233fd0415b5085e8c319e0d1b1faf
Elzoka <mahmoud_elzoka@yahoo.com>
2018-12-07 23:14:15 +0200

    Connecting to twillo api

--- RESTfulAPI/lib/config.js
+++ RESTfulAPI/lib/config.js
@@ -15,7 +15,12 @@ environments.staging = {
     httpsPort: 3001,
     envName: 'staging',
     hashingSecret: 'thisIsASecret',
-    maxChecks: 5
+    maxChecks: 5,
+    'twilio' : {
+        'accountSid' : 'ACb32d411ad7fe886aac54c665d25e5c5d',
+        'authToken' : '9455e3eb3109edc12e3d8c92768f7a67',
+        'fromPhone' : '+15005550006'
+    }
 };
 
 // Production environment
@@ -24,7 +29,12 @@ environments.production = {
     httpsPort: 5001,
     envName: 'production',
     hashingSecret: 'thisIsASecret',
-    maxChecks: 5
+    maxChecks: 5,
+    twilio : {
+        'accountSid' : 'ACbee38596ad59ea838405284a4d62e139',
+        'authToken' : '158320f061029de518ad1f6f809fdeb2',
+        'fromPhone' : '+17752776241'
+    }
 }
 
 // Determine which environment was passed as a command-line argument
--- RESTfulAPI/lib/helpers.js
+++ RESTfulAPI/lib/helpers.js
@@ -6,6 +6,8 @@
 
 // Dependencies
 const crypto = require('crypto');
+const querystring = require('querystring');
+const https = require('https');
 
 const config = require('./config');
 
@@ -53,9 +55,61 @@ helpers.createRandomString = (strLength) =>{
     }
 }
 
+helpers.sendTwilioSms = (phone, msg, callback) => {
+    // Validate the parameters
+    // Send an SMS message via twilio
+    phone = typeof phone == 'string' && phone.trim().length == 10 ? phone.trim() : null;
+    msg = typeof msg == 'string' && msg.length > 0 && msg.trim().length <= 1600 ? msg.trim() : null;
+    if(phone && msg){
+        // Configure the request payload
+        const payload = {
+            'From': config.twilio.fromPhone,
+            'To': '+20'+ phone,
+            'Body': msg
+        };
 
+        // Stringify the payload
+        const stringPayload = querystring.stringify(payload);
+        // Configure the request details
+        const requestDetails = {
+            'protocol': "https:",
+            'hostname': 'api.twilio.com',
+            'method': 'POST',
+            'path' : `/2010-04-01/Accounts/${config.twilio.accountSid}/Messages.json`,
+            'auth': `${config.twilio.accountSid}:${config.twilio.authToken}`,
+            'headers': {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Content-Length': Buffer.byteLength(stringPayload)
+            }
+        }
+        
+        // Inistantiate the request object
+        const req = https.request(requestDetails, (res) => {
+            // Grab the status of the sent request
+            const status = res.statusCode;
+            // Callback successfully if request went through
+            if(status == 200 || status == 201){
+                callback(null);
+            }else {
+                callback('Status code returned was '+ status);
+                console.log(res);
+            }
+        });
 
+        // Bind to the error event so it doesn't get thrown
+        req.on('error', (e) => {
+            callback(e);
+        });
 
+        // Add the payload
+        req.write(stringPayload);
+
+        // End the request
+        req.end;
+    }else{
+        callback('Given parameters were missing or invalid');
+    }
+};
 
 
 // Export the module
