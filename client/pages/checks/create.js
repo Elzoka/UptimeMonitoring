3177facec4b24e71f4845454835c868142ba5e56
eslamkhalefa <eslamkhalefa19@gmail.com>
2021-02-05 00:20:01 +0200

    handle form data with formik

--- client/pages/checks/create.js
+++ client/pages/checks/create.js
@@ -1,5 +1,21 @@
+import { useFormik } from "formik";
 import Link from "next/link";
+
 const CreateCheck = () => {
+  const formik = useFormik({
+    initialValues: {
+      protocol: "http",
+      url: "",
+      method: "get",
+      timeout: 1,
+      successCodes: [],
+    },
+
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
   return (
     <div className="content">
       <h1>Create A Check</h1>
@@ -10,107 +26,77 @@ const CreateCheck = () => {
 
       <hr />
       <div className="formWrapper">
-        <form id="checksCreate" action="/api/checks" method="POST">
+        <form id="checksCreate" onSubmit={formik.handleSubmit}>
           <div className="formError" />
           <div className="inputWrapper">
             <div className="inputLabel">Protocol</div>
-            <select name="protocol">
-              <option value="http" selected>
-                http://
-              </option>
+            <select
+              name="protocol"
+              value={formik.values.protocol}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+            >
+              <option value="http">http://</option>
               <option value="https">https://</option>
             </select>
           </div>
           <div className="inputWrapper">
             <div className="inputLabel">URL (not including protocol)</div>
-            <input type="text" name="url" placeholder="mywebsite.com/my/path" />
+            <input
+              type="text"
+              name="url"
+              placeholder="mywebsite.com/my/path"
+              value={formik.values.url}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+            />
           </div>
           <div className="inputWrapper">
             <div className="inputLabel">
               Success Codes (HTTP codes that should represent {"up"})
             </div>
             <div className="checkboxGroup">
-              <input
-                type="checkbox"
-                className="multiselect intval"
-                name="successCodes"
-                defaultValue={200}
-                defaultChecked
-              />
-              200
-              <input
-                type="checkbox"
-                className="multiselect intval"
-                name="successCodes"
-                defaultValue={201}
-                defaultChecked
-              />
-              201
-              <input
-                type="checkbox"
-                className="multiselect intval"
-                name="successCodes"
-                defaultValue={301}
-              />
-              301
-              <input
-                type="checkbox"
-                className="multiselect intval"
-                name="successCodes"
-                defaultValue={302}
-              />
-              302
-              <input
-                type="checkbox"
-                className="multiselect intval"
-                name="successCodes"
-                defaultValue={400}
-              />
-              400
-              <input
-                type="checkbox"
-                className="multiselect intval"
-                name="successCodes"
-                defaultValue={403}
-              />
-              403
-              <input
-                type="checkbox"
-                className="multiselect intval"
-                name="successCodes"
-                defaultValue={404}
-              />
-              404
-              <input
-                type="checkbox"
-                className="multiselect intval"
-                name="successCodes"
-                defaultValue={406}
-              />
-              406
-              <input
-                type="checkbox"
-                className="multiselect intval"
-                name="successCodes"
-                defaultValue={500}
-              />
-              500
+              {[200, 201, 301, 302, 400, 403, 404, 406, 500].map(
+                (successCode) => (
+                  <>
+                    <input
+                      type="checkbox"
+                      className="multiselect intval"
+                      name="successCodes"
+                      value={successCode}
+                      onChange={formik.handleChange}
+                      onBlur={formik.handleBlur}
+                      //   defaultValue={successCode}
+                    />
+                    {successCode}
+                  </>
+                )
+              )}
             </div>
           </div>
           <div className="inputWrapper">
             <div className="inputLabel">HTTP Method</div>
-            <select name="httpmethod">
+            <select
+              name="method"
+              value={formik.values.method}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+            >
               <option value="post">POST</option>
-              <option value="get" selected>
-                GET
-              </option>
+              <option value="get">GET</option>
               <option value="put">PUT</option>
               <option value="delete">DELETE</option>
             </select>
           </div>
           <div className="inputWrapper">
             <div className="inputLabel">Timeout</div>
-            <select className="intval" name="timeoutSeconds">
+            <select
+              className="intval"
+              name="timeout"
+              value={formik.values.timeout}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+            >
               <option value={1}>1 second</option>
               <option value={2}>2 seconds</option>
               <option value={3}>3 seconds</option>
