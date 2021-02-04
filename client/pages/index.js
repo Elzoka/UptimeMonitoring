b2654f5ab713fc9724d40ed0147c994379744e73
eslamkhalefa <eslamkhalefa19@gmail.com>
2021-02-04 23:23:24 +0200

    fix styling index

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,27 +2,29 @@ import Link from "next/link";
 
 function HomePage() {
   return (
-    <div className="content">
-      <h1>Uptime Monitoring</h1>
-      <h2>Made Simple</h2>
-      <div className="blurb">
-        We offer free, simple uptime monitoring for HTTP/HTTPS sites of all
-        kinds, When your site goes down, we ll send you a text to let you know.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio obcaecati
-        voluptatum aliquid ab odit adipisci nulla consectetur nihil, cumque eius
-        tempora reiciendis hic molestiae possimus facere eum quis corrupti
-        pariatur. Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-        Voluptatum earum et non nulla nisi, perspiciatis, magni, natus minima
-        aut eius incidunt unde error labore nemo laboriosam illo dignissimos
-        provident architecto?
-      </div>
-      <div className="ctaWrapper">
-        <Link href="/signup">
-          <a className="cta green">Get Started</a>
-        </Link>
-        <Link href="/login">
-          <a className="cta blue">Login</a>
-        </Link>
+    <div className="index">
+      <div className="content">
+        <h1>Uptime Monitoring</h1>
+        <h2>Made Simple</h2>
+        <div className="blurb">
+          We offer free, simple uptime monitoring for HTTP/HTTPS sites of all
+          kinds, When your site goes down, we ll send you a text to let you
+          know. Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
+          obcaecati voluptatum aliquid ab odit adipisci nulla consectetur nihil,
+          cumque eius tempora reiciendis hic molestiae possimus facere eum quis
+          corrupti pariatur. Lorem ipsum, dolor sit amet consectetur adipisicing
+          elit. Voluptatum earum et non nulla nisi, perspiciatis, magni, natus
+          minima aut eius incidunt unde error labore nemo laboriosam illo
+          dignissimos provident architecto?
+        </div>
+        <div className="ctaWrapper">
+          <Link href="/signup">
+            <a className="cta green">Get Started</a>
+          </Link>{" "}
+          <Link href="/login">
+            <a className="cta blue">Login</a>
+          </Link>
+        </div>
       </div>
     </div>
   );
